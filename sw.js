29e255662d0aae793a8d39149c215317b4018c04
Kaushalji <harshitkaushal9129@gmail.com>
2026-02-06 16:50:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,19 @@
 const CACHE_NAME = 'devstudio-v1';
-const assetsToCache = [
+const assets = [
   './',
   './index.html',
   './manifest.json',
   './Kaushalji.png'
 ];
 
-// Service Worker Install and Caching
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching assets...');
-      return cache.addAll(assetsToCache);
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-// Fetching assets from cache (Offline Support)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate event to clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Clearing old cache...');
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
